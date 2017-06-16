2d2197677e33f2bcc1ad0d006263b95a3a14ed6c
rti7743 <super.rti@gmail.com>
2017-06-16 09:36:09 +0900

    raspberry piでのビルド

--- build.sh
+++ build.sh
@@ -3,20 +3,31 @@
 if [ "x$1" != "x" ];then
 	export BUILD_ARCH=$1
 else
-	if uname -a | grep arm ; then
+	if uname -a | grep -e beagle -e fhc ; then
 		export BUILD_ARCH=arm
+		FPUHINT="-O3 -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp -mabi=aapcs-linux -fforce-addr -fomit-frame-pointer -fstrength-reduce -ftree-vectorize -mvectorize-with-neon-quad"
+		V8MAKENAME="$BUILD_ARCH"
+	elif uname -a | grep raspberrypi ; then
+		export BUILD_ARCH=arm
+		FPUHINT="-O3 -mfloat-abi=hard -mthumb -marm"
+		V8MAKENAME="$BUILD_ARCH.release hardfp=on"
+	elif uname -a | grep arm ; then
+		export BUILD_ARCH=arm
+		FPUHINT="-O3"
+		V8MAKENAME="$BUILD_ARCH"
 	elif uname -a | grep x86_64 ; then
 		export BUILD_ARCH=x64
+		FPUHINT="-O3"
+		V8MAKENAME="$BUILD_ARCH"
 	elif uname -a | grep x86 ; then
 		export BUILD_ARCH=ia32
+		FPUHINT="-O3"
+		V8MAKENAME="$BUILD_ARCH"
 	else
 		echo "architecture is unknown!"
 		exit;
 	fi
 fi
-#export BUILD_ARCH=arm
-#FPUHINT="-O3 -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp -mabi=aapcs-linux -fforce-addr -fomit-frame-pointer -fstrength-reduce -ftree-vectorize -mvectorize-with-neon-quad"
-FPUHINT="$2"
 
 O_CFLAGS="$CFLAGS"
 O_CXXFLAGS="$CXXFLAGS"
@@ -46,11 +57,11 @@ make
 cd ../..
 
 cd v8
-CXXFLAGS="$FPUHINT -Wno-strict-overflow -Wno-unused-local-typedefs -Wno-aggressive-loop-optimizations"
+export CXXFLAGS="$FPUHINT -Wno-strict-overflow -Wno-unused-local-typedefs -Wno-aggressive-loop-optimizations"
 chmod +x build/gyp/gyp
 make clean
-make $BUILD_ARCH
-CXXFLAGS="$CFLAGS $FPUHINT"
+make $BUILD_ARCH.release hardfp=on
+export CXXFLAGS="$CFLAGS $FPUHINT"
 cd ..
 
 cd pcre
